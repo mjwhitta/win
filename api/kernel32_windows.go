309d9ea090fe592b44b35e8d25c98344f2f26e48
Miles Whittaker <mj@whitta.dev>
2024-06-04 11:00:36 -0400

    Added some heap functions from heapapi.h

--- api/kernel32_windows.go
+++ api/kernel32_windows.go
@@ -5,10 +5,82 @@ package api
 import (
 	"golang.org/x/sys/windows"
 
+	"github.com/mjwhitta/errors"
 	"github.com/mjwhitta/win/types"
 )
 
 var kernel32 *windows.LazyDLL = windows.NewLazySystemDLL("kernel32")
+
+// HeapAlloc is from heapapi.h
+func HeapAlloc(
+	heapHndl uintptr,
+	dwFlags uintptr,
+	dwBytes int,
+) (uintptr, error) {
+	var e error
+	var addr uintptr
+	var proc string = "HeapAlloc"
+
+	addr, _, e = kernel32.NewProc(proc).Call(
+		heapHndl,
+		dwFlags,
+		uintptr(dwBytes),
+	)
+	if addr == 0 {
+		return 0, errors.Newf("%s: %w", proc, e)
+	}
+
+	return addr, nil
+}
+
+// HeapCreate is from heapapi.h
+func HeapCreate(
+	flOptions uintptr,
+	dwInitialSize int,
+	dwMaximumSize int,
+) (uintptr, error) {
+	var e error
+	var hndl uintptr
+	var proc string = "HeapCreate"
+
+	hndl, _, e = kernel32.NewProc(proc).Call(
+		flOptions,
+		uintptr(dwInitialSize),
+		uintptr(dwMaximumSize),
+	)
+	if hndl == 0 {
+		return 0, errors.Newf("%s: %w", proc, e)
+	}
+
+	return hndl, nil
+}
+
+// HeapDestroy is from heapapi.h
+func HeapDestroy(hndl uintptr) error {
+	var e error
+	var ok uintptr
+	var proc string = "HeapDestroy"
+
+	if ok, _, e = kernel32.NewProc(proc).Call(hndl); ok == 0 {
+		return errors.Newf("%s: %w", proc, e)
+	}
+
+	return nil
+}
+
+// HeapFree is from heapapi.h
+func HeapFree(heapHndl uintptr, dwFlags uintptr, addr uintptr) error {
+	var e error
+	var ok uintptr
+	var proc string = "HeapFree"
+
+	ok, _, e = kernel32.NewProc(proc).Call(heapHndl, dwFlags, addr)
+	if ok == 0 {
+		return errors.Newf("%s: %w", proc, e)
+	}
+
+	return nil
+}
 
 // OutputDebugStringW will print a string that Dbgview.exe and
 // dbgview64.exe will display. Useful for debugging DLLs.
--- globals.go
+++ globals.go
@@ -1,4 +1,4 @@
 package win
 
 // Version is the package version
-const Version string = "0.24.1"
+const Version string = "0.24.2"
