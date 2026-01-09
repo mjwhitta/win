74a7afe88838b6914bb6e245dbb7b71798323e11
Miles Whittaker <mj@whitta.dev>
2026-01-09 16:45:38 -0500

    Added GIF support to setwall

--- cmd/setwall/main_windows.go
+++ cmd/setwall/main_windows.go
@@ -3,6 +3,15 @@
 package main
 
 import (
+	"image/gif"
+	"image/png"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+
 	"github.com/mjwhitta/cli"
 	"github.com/mjwhitta/log"
 	"github.com/mjwhitta/win/desktop"
@@ -42,7 +51,85 @@ func main() {
 		img = cli.Arg(0)
 	}
 
+	if strings.HasSuffix(img, ".gif") {
+		if e = wallpaperSlideshow(img); e != nil {
+			panic(e)
+		}
+
+		img = ""
+	}
+
 	if e = desktop.SetWallpaper(img, styles[flags.style]); e != nil {
 		panic(e)
+	}
+}
+
+//nolint:wrapcheck // Not wrapping errors in a main package
+func wallpaperSlideshow(img string) (e error) {
+	var fn string = filepath.Join("c:/", "windows", "temp", "mw.png")
+	var fr *os.File
+	var fw *os.File
+	var g *gif.GIF
+	var sig chan os.Signal = make(chan os.Signal, 1)
+
+	if fr, e = os.Open(filepath.Clean(img)); e != nil {
+		return e
+	}
+	defer func() {
+		if e2 := fr.Close(); (e == nil) && (e2 != nil) {
+			e = e2
+		}
+	}()
+
+	if g, e = gif.DecodeAll(fr); e != nil {
+		return e
+	}
+
+	// Setup SIGINT for stopping
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		for i := range g.Image {
+			select {
+			case <-sig:
+				signal.Stop(sig)
+
+				_ = os.Remove(fn)
+
+				return nil
+			default:
+				// Write frame to c:/windows/temp
+				if fw, e = os.Create(filepath.Clean(fn)); e != nil {
+					return e
+				}
+
+				e = png.Encode(
+					fw,
+					g.Image[i].SubImage(g.Image[i].Bounds()),
+				)
+				if e != nil {
+					_ = fw.Close()
+					return e
+				}
+
+				if e = fw.Close(); e != nil {
+					return e
+				}
+
+				// Set wallpaper to frame
+				e = desktop.SetWallpaper(fn, styles[flags.style])
+				if e != nil {
+					panic(e)
+				}
+
+				// Sleep for the specified delay
+				//
+				// NOTE: This is supposed to be in 100ths of a second,
+				// but looks to actually be milliseconds.
+				time.Sleep(
+					time.Duration(g.Delay[i]) * time.Millisecond,
+				)
+			}
+		}
 	}
 }
--- globals.go
+++ globals.go
@@ -1,4 +1,4 @@
 package win
 
 // Version is the package version
-const Version string = "0.31.3"
+const Version string = "0.31.4"
